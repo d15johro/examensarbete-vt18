54e10c76c9d4847ef454f6aeae14703c3e659495
Johan Rönkkö <d15johro@student.his.se>
2018-02-27 16:31:55 +0100

    setting up client and server

---
+++ websocket/client/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/d15johro/examensarbete-vt18-correct/websocket/codec"
+	"github.com/d15johro/examensarbete-vt18-correct/websocket/pb_send"
+
+	"golang.org/x/net/websocket"
+)
+
+var (
+	url    = flag.String("url", "ws://localhost:8080/websocket", "websocket server url")
+	origin = flag.String("origin", "http://localhost/", "websocket origin")
+)
+
+type client struct {
+	conn    *websocket.Conn
+	request chan bool
+	message message
+}
+
+type message struct {
+	ID int32 `json:"id"`
+}
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	conn, err := websocket.Dial(*url, "", *origin)
+	if err != nil {
+		log.Fatalln("could not dial websocket server:", err)
+	}
+	defer conn.Close()
+	c := client{
+		conn:    conn,
+		request: make(chan bool),
+		message: message{ID: 0},
+	}
+	defer close(c.request)
+	go func() { c.request <- true }() // init request
+	go c.write()
+	c.read()
+}
+
+func (c *client) read() {
+	defer c.conn.Close()
+	for {
+		var x pb_send.Send
+		if err := codec.PB.Receive(c.conn, &x); err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("%d: %s\n", c.message.ID, x.Data)
+		c.request <- true
+	}
+}
+
+func (c *client) write() {
+	defer c.conn.Close()
+	for range c.request {
+		if c.message.ID >= 30 {
+			break
+		}
+		if err := websocket.JSON.Send(c.conn, c.message); err != nil {
+			log.Println("write:", err)
+			break
+		}
+		c.message.ID++
+	}
+}
---
+++ websocket/server/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/d15johro/examensarbete-vt18-correct/websocket/codec"
+	"github.com/d15johro/examensarbete-vt18-correct/websocket/pb_send"
+	"golang.org/x/net/websocket"
+)
+
+var addr = flag.String("addr", ":8080", "the address to host the websocket server")
+
+type client struct {
+	conn *websocket.Conn
+	send chan message
+}
+
+type message struct {
+	ID int32 `json:"id"`
+}
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	http.Handle("/websocket", websocket.Handler(func(conn *websocket.Conn) {
+		defer conn.Close()
+		c := client{
+			conn: conn,
+			send: make(chan message),
+		}
+		defer func() { close(c.send) }()
+		go c.write()
+		c.read()
+	}))
+	log.Fatalln(http.ListenAndServe(*addr, nil))
+}
+
+func (c *client) read() {
+	defer c.conn.Close()
+	for {
+		var msg message
+		if err := websocket.JSON.Receive(c.conn, &msg); err != nil {
+			log.Println("read:", err)
+			break
+		}
+		c.send <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.conn.Close()
+	for msg := range c.send {
+		if msg.ID%10 == 0 {
+			log.Println("message ID:", msg.ID)
+		}
+		send := pb_send.Send{Data: "some chunk of text"}
+		if err := codec.PB.Send(c.conn, &send); err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
