a3aa45d72fd76bd57537c0cf8cf0310192c4cc05
Johan Rönkkö <d15johro@student.his.se>
2018-02-27 19:01:27 +0100

    refactoring and commenting code for easier interpretation

--- websocket/client/main.go
+++ websocket/client/main.go
@@ -25,17 +25,21 @@ const (
 )
 
 type client struct {
-	conn    *websocket.Conn
+	// conn is the websocket connection to which the client is connected.
+	conn *websocket.Conn
+	// request is a channel used to syncronize read and write operations (read --> write).
 	request chan bool
 	message message
 }
 
+// message represents a message holding an ID that tells the server what data to send.
 type message struct {
 	ID int32 `json:"id"`
 }
 
 func init() {
 	flag.Parse()
+	// TODO: clean log file with metrics data.
 }
 
 func main() {
@@ -50,14 +54,19 @@ func main() {
 		message: message{ID: 0},
 	}
 	defer close(c.request)
-	go func() { c.request <- true }() // init request
-	go c.write()
+	go func() { c.request <- true }() // init write operation
+	go c.write()                      // spawn write function on a different user-space thread
 	c.read()
 }
 
+// read reads data received from the server. Once the data is read and
+// deserialized, read append all collected metrics to a log file.
+// read then sends a bool to the client.request channel which triggers
+// the write function to start operating.
 func (c *client) read() {
 	defer c.conn.Close()
 	for {
+		// read data from server:
 		var buf = make([]byte, bufferReadSize)
 		n, err := c.conn.Read(buf)
 		if err != nil {
@@ -66,7 +75,8 @@ func (c *client) read() {
 		}
 		data := buf[:n]                                                      // skip zeros
 		responseTimeDuration := time.Since(startAccessTime).Seconds() * 1000 // response time in ms
-		startDeserializationTime := time.Now()                               // start deserialization time clock
+		// deserialize data:
+		startDeserializationTime := time.Now() // start deserialization time clock
 		var x pb_send.Send
 		if err = codec.PB.Unmarshal(data, websocket.BinaryFrame, &x); err != nil {
 			log.Println("read:", err)
@@ -74,7 +84,7 @@ func (c *client) read() {
 		}
 		deserializationDuration := time.Since(startDeserializationTime).Seconds() * 1000 // deserialization time in ms
 		accessTimeDuration := time.Since(startAccessTime).Seconds() * 1000               // access time in ms
-
+		// TODO: save metrics to a log file.
 		log.Printf("\n---\nmetrics:\n\tID: %d\n\taccess time: %fms\n\tresponse time %fms\n\tdeserialization time: %fms\n\tmessage: %s\n\tbytes read (data size): %d\n---",
 			c.message.ID,
 			accessTimeDuration,
@@ -87,10 +97,11 @@ func (c *client) read() {
 	}
 }
 
+// write sends a message to server requesting more data.
 func (c *client) write() {
 	defer c.conn.Close()
 	for range c.request {
-		if c.message.ID >= 5 {
+		if c.message.ID >= 5 { // limit requests
 			break
 		}
 		c.message.ID++
@@ -99,6 +110,5 @@ func (c *client) write() {
 			log.Println("write:", err)
 			break
 		}
-
 	}
 }
--- websocket/server/main.go
+++ websocket/server/main.go
@@ -14,7 +14,9 @@ import (
 var addr = flag.String("addr", ":8080", "the address to host the websocket server")
 
 type client struct {
+	// conn is the websocket connection to which the client connects.
 	conn *websocket.Conn
+	// send is a channel used to syncronize read and write operations (read --> write).
 	send chan message
 }
 
@@ -24,6 +26,7 @@ type message struct {
 
 func init() {
 	flag.Parse()
+	// TODO: clean log file with metrics data.
 }
 
 func main() {
@@ -34,12 +37,15 @@ func main() {
 			send: make(chan message),
 		}
 		defer func() { close(c.send) }()
-		go c.write()
+		go c.write() // spawn write function on a different user-space thread
 		c.read()
 	}))
 	log.Fatalln(http.ListenAndServe(*addr, nil))
 }
 
+// read reads messages received from the client. Once the message is read and
+// deserialized, read sends the message to the client.send channel which triggers
+// the write function to start operating.
 func (c *client) read() {
 	defer c.conn.Close()
 	for {
@@ -52,22 +58,29 @@ func (c *client) read() {
 	}
 }
 
+// write writes and sends data to the client depending on the ID of the message
+// being sent to the client.send channel that triggered the write operation.
+// Collected metrics are appended to a log file.
 func (c *client) write() {
 	defer c.conn.Close()
 	for msg := range c.send {
-		startSerializationTime := time.Now()
-		x := pb_send.Send{Data: "some chunk of text"}
+		// TODO: select what data to send depending on msg.ID
+		x := pb_send.Send{Data: "some chunk of text"} // hardcoded
+		// serialize data structure:
+		startSerializationTime := time.Now() // start serialization time clock
 		data, _, err := codec.PB.Marshal(&x)
 		if err != nil {
 			log.Println("write:", err)
 			break
 		}
 		serializationDuration := time.Since(startSerializationTime).Seconds() * 1000 // serialization time in ms
+		// write data to client:
 		n, err := c.conn.Write(data)
 		if err != nil {
 			log.Println("write:", err)
 			break
 		}
+		// TODO: append data to a log file.
 		log.Printf("\n---\nmetrics:\n\tID: %d\n\tserialization time: %f ms\n\tbytes written: %d\n---",
 			msg.ID,
 			serializationDuration,
