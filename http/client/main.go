46d96518784617c5dd930c85e3060ac74a0e4982
Johan Rönkkö <d15johro@student.his.se>
2018-03-08 13:22:10 +0100

    updating way to measure deserialization of fbs

--- http/client/main.go
+++ http/client/main.go
@@ -90,12 +90,10 @@ func main() {
 				break
 			}
 		case "fbs":
-			// Unlike pb, deserializing fbs basically means storing the raw binary data in a struct.
-			// Therefore, the deserialization time for fbs will probably always be 0ms.
-			offset := flatbuffers.UOffsetT(0)
-			n := flatbuffers.GetUOffsetT(data[offset:])
-			osm := &fbs.OSM{}
-			osm.Init(data, n+offset)
+			if err := deserializeFbs(data); err != nil {
+				log.Println(err)
+				break
+			}
 		default:
 			log.Fatalln("serialization format not supported")
 		}
@@ -136,4 +134,141 @@ func (m *metrics) setup() error {
 	}
 	_, err = os.Create(m.filepath)
 	return err
+}
+
+// Unlike pb, deserializing fbs basically means storing the raw binary data in a struct.
+// Therefore, the deserialization time for fbs will probably always be 0ms. Since maps
+// want access to all data we measure deserialization for fbs as time it takes to access all data
+// from memory. This means that we have to call all the getter methods for all elements in the map.
+func deserializeFbs(data []byte) error {
+	offset := flatbuffers.UOffsetT(0)
+	n := flatbuffers.GetUOffsetT(data[offset:])
+	osm := &fbs.OSM{}
+	// osm attributes:
+	osm.Init(data, n+offset)
+	osm.Attribution()
+	osm.Copyright()
+	osm.Generator()
+	osm.License()
+	osm.Version()
+	// nodes:
+	for i := 0; i < osm.NodesLength(); i++ {
+		// node:
+		var node fbs.Node
+		ok := osm.Nodes(&node, i)
+		if !ok {
+			return fmt.Errorf("bad deserialization")
+		}
+		// node attributes:
+		node.Lat()
+		node.Lon()
+		// shared attributes:
+		var sharedAttributes fbs.SharedAttributes
+		sA := node.SharedAttributes(&sharedAttributes)
+		sA.Version()
+		sA.User()
+		sA.Uid()
+		sA.Timestamp()
+		sA.Id()
+		sA.Changeset()
+		// tags:
+		for j := 0; j < node.TagsLength(); j++ {
+			// tag:
+			var tag fbs.Tag
+			ok := node.Tags(&tag, j)
+			if !ok {
+				return fmt.Errorf("bad deserialization")
+			}
+			//tag attributes:
+			tag.Key()
+			tag.Value()
+		}
+	}
+	// ways:
+	for i := 0; i < osm.WaysLength(); i++ {
+		// way:
+		var way fbs.Way
+		ok := osm.Ways(&way, i)
+		if !ok {
+			return fmt.Errorf("bad deserialization")
+		}
+		// shared attributes:
+		var sharedAttributes fbs.SharedAttributes
+		sA := way.SharedAttributes(&sharedAttributes)
+		sA.Version()
+		sA.User()
+		sA.Uid()
+		sA.Timestamp()
+		sA.Id()
+		sA.Changeset()
+		// nds:
+		for j := 0; j < way.NdsLength(); j++ {
+			// nd:
+			var nd fbs.Nd
+			ok := way.Nds(&nd, j)
+			if !ok {
+				return fmt.Errorf("bad deserialization")
+			}
+			// nd attributes:
+			nd.Ref()
+		}
+		// tags:
+		for j := 0; j < way.TagsLength(); j++ {
+			// tag:
+			var tag fbs.Tag
+			ok := way.Tags(&tag, j)
+			if !ok {
+				return fmt.Errorf("bad deserialization")
+			}
+			//tag attributes:
+			tag.Key()
+			tag.Value()
+		}
+	}
+	// relations
+	for i := 0; i < osm.RelationsLength(); i++ {
+		// relation:
+		var relation fbs.Relation
+		ok := osm.Relations(&relation, i)
+		if !ok {
+			return fmt.Errorf("bad deserialization")
+		}
+		// relation attributes:
+		relation.Visible()
+		// shared attributes:
+		var sharedAttributes fbs.SharedAttributes
+		sA := relation.SharedAttributes(&sharedAttributes)
+		sA.Version()
+		sA.User()
+		sA.Uid()
+		sA.Timestamp()
+		sA.Id()
+		sA.Changeset()
+		// members:
+		for j := 0; j < relation.MembersLength(); j++ {
+			// member:
+			var member fbs.Member
+			ok := relation.Members(&member, j)
+			if !ok {
+				return fmt.Errorf("bad deserialization")
+			}
+			// member attributes:
+			member.Ref()
+			member.Role()
+			member.Type()
+		}
+		// tags:
+		for j := 0; j < relation.TagsLength(); j++ {
+			// tag:
+			var tag fbs.Tag
+			ok := relation.Tags(&tag, j)
+			if !ok {
+				return fmt.Errorf("bad deserialization")
+			}
+			//tag attributes:
+			tag.Key()
+			tag.Value()
+		}
+	}
+	return nil
 }
